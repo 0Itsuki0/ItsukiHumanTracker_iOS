40fb39024bdcadb71b4b68901a4b152662c73a15
itsuki <itsuki.enjyoy@gmail.com>
2024-07-31 10:18:27 +0900

    Initial Commit

---
+++ ItsukiTracker/ItsukiTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ItsukiTrackerApp.swift
+//  ItsukiTracker
+//
+//  Created by Itsuki on 2024/07/31.
+//
+
+import SwiftUI
+
+@main
+struct ItsukiTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
